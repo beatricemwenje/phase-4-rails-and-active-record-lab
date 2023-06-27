af34f5cf6ef19dfd2a88ad6591246a8ffcbbd4f4
beatrice <beatrice.mwenje@student.moringaschool>
2023-06-27 09:34:14 +0300

    Done with assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#create a student instance
+Student.create(first_name: "Beatrice", last_name: "Mwenje", grade: 180)
